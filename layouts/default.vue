4c918bafa80631c2ae11126d14519eb594f05e97
kyo <smilebowl-trump27@yahoo.co.jp>
2019-07-25 20:43:43 +0900

    set transition

--- layouts/default.vue
+++ layouts/default.vue
@@ -82,3 +82,12 @@ export default {
   }
 }
 </script>
+
+<style>
+.page-enter-active, .page-leave-active {
+  transition: opacity 0.5s ease-out
+}
+.page-enter, .page-leave-active {
+  opacity: 0
+}
+</style>
